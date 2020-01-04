61a63df35b48b957fc9ba5fba9315fafb2c3abff
andreasantarl <andrea.santarl@gmail.com>
2020-01-04 17:03:50 -0600

    Adds email module to project, but won't work with gmail

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pprint
+import sys
+import settings
+from emails import send_email
 
 exclusion_list = ['http://www.nytimes.com/services/mobile/index.html', 'https://www.nytimes.com/subscription/cooking.html']
 url = "https://nytimes.com"
+
 webpage = requests.get(url)
 page_contents = BeautifulSoup(webpage.content, 'lxml')
+
 articles = page_contents.find_all('a')
+if not articles:
+    print("Nothing found")
+    sys.exit()
 
 front_page_articles = []
-
 
 for article in articles:
     if '.html' in article.attrs['href'] and article.attrs['href'] not in exclusion_list:
@@ -21,3 +28,4 @@ for article in articles:
 
 pprint.pprint(set(front_page_articles), indent=2)
 
+send_email()
---
+++ emails.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+import os
+
+
+def send_email():
+    sender_email_address = os.getenv("EMAIL_ADDRESS")
+    sender_email_password = os.getenv("EMAIL_PASSWORD")
+    with smtplib.SMTP("smtp.gmail.com", 587) as s:
+        try:
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(sender_email_address, sender_email_password)
+            message = "Things need ot be said"
+            s.sendmail(sender_email_address, "andrea.santarl@gmail.com", message)
+        except smtplib.SMTPAuthenticationError as e:
+            print(e)
+
+
+
